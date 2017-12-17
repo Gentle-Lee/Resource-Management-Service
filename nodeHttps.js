e782cafa34c0f7b1a3171108ed048a96beb22477
Gentle_Lee <tommy007lee@gmail.com>
2017-12-17 17:28:34 +0800

    test

--- nodeHttps.js
+++ nodeHttps.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const options = {
   pfx: fs.readFileSync('fixtures/api.gentleleetommy.cn.pfx'),
-  passphrase: 'password'
+  passphrase: '83508089l'
 };
 
 https.createServer(options, (req, res) => {
