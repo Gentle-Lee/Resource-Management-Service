4225a8a03ae58f6da8c0cc63ffffe5a25ff10ad7
Gentle_Lee <tommy007lee@gmail.com>
2017-12-17 16:19:44 +0800

    test

--- server.js
+++ server.js
@@ -2,36 +2,15 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
-var Client = require('mysql').Client;
-var client = new Client();
+var mysql  = require('mysql');  
 
-client.user = 'root';
-client.password = '83508089l';
-
-console.log('Connecting to MySQL...');
-
-client.query('USE Project');     //如果MySQL中没有库表，赶紧建。
-
-http = require("http");
-
-var server = http.createServer(function(request, response) {
-    response.writeHeader(200, {"Content-Type": "text/html"});
-
-    client.query('SELECT * FROM test', function selectCb(err, results, fields) {  
-        if (err) {  
-            throw err;  
-        }  
-
-        var data = '';
-        for (var i=0; i<results.length; i++) {          
-            var firstResult = results[i];
-            data += 'id: ' + firstResult['id']+'tag: ' + firstResult['user_name']; 
-        } 
-
-        response.write(data); 
-        response.end();
-    });
-});
+var connection = mysql.createConnection({     
+		host     : 'localhost',       
+		user     : 'root',              
+		password : '83508089l',       
+		port: '3306',                   
+		database: 'Project', 
+}); 
 
 app.get('/listUsers', function (req, res) {
 	res.writeHead(200, {"Content-Type": "text/html"});
