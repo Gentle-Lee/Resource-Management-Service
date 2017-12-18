02181536e8f98f0b02c5b7eac6f2300bff9a1c47
Gentle-Lee <tommy007lee@gmail.com>
2017-12-19 04:07:58 +0800

    server init

--- server/server.js
+++ server/server.js
@@ -1,45 +1,78 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
+// 引用 express 来支持 HTTP Server 的实现
+const express = require('express');
+var mysql = require('mysql');
 
-var mysql  = require('mysql');  
-
-var connection = mysql.createConnection({     
-		host     : 'localhost',       
-		user     : 'root',              
-		password : '83508089l',       
-		port: '3306',                   
-		database: 'Project', 
-}); 
+// 创建一个 express 实例
+const app = express();
 
 app.use('/listUsers', (request, response, next) => {
-    response.writeHead(200, {"Content-Type": "text/html"});
-        connection.connect();
+      var connection = mysql.createConnection({
+           host     : 'localhost',
+           user     : 'root',
+           password : '83508089l',
+           port: '3306',
+           database : 'Project'
+      });
+      response.writeHead(200, {"Content-Type": "text/html"});
+      connection.connect();
 
-        var  sql = 'SELECT * FROM test';
-        //查
-        connection.query(sql,function (err, result) {
-                if(err){
-                  console.log('[SELECT ERROR] - ',err.message);
-                  return;
-                        }
+      var  sql = 'SELECT * FROM test';
+      //查
+      connection.query(sql,function (err, result) {
+              if(err){
+                console.log('[SELECT ERROR] - ',err.message);
+                return;
+               }
+             response.end(JSON.stringify(result),'utf-8');
 
-                        response.write(JSON.stringify(result));
-                        response.end();
+             console.log('--------------------------SELECT----------------------------');
+             console.log(result);
+             console.log('------------------------------------------------------------\n\n');
+      });
 
-               console.log('--------------------------SELECT----------------------------');
-               console.log(res);
-               console.log('------------------------------------------------------------\n\n');
-        });
-
-        connection.end();
+      connection.end();
 });
 
-var server = app.listen(8081, function () {
+app.use('/listTeam', (request, response, next) => {
+      var connection = mysql.createConnection({
+           host     : 'localhost',
+           user     : 'root',
+           password : '83508089l',
+           port: '3306',
+           database : 'Project'
+      });
+      response.writeHead(200, {"Content-Type": "text/html"});
+      connection.connect();
 
-  var host = server.address().address
-  var port = server.address().port
+      var  sql = 'SELECT * FROM team';
+      //查
+      connection.query(sql,function (err, result) {
+              if(err){
+                console.log('[SELECT ERROR] - ',err.message);
+                return;
+               }
+             response.end(JSON.stringify(result),'utf-8');
 
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+             console.log('--------------------------SELECT----------------------------');
+             console.log(result);
+             console.log('------------------------------------------------------------\n\n');
+      });
 
-})
+      connection.end();
+});
+
+app.use('/test', (request, response, next) => {
+      
+      response.writeHead(200, {"Content-Type": "text/html"});
+
+
+     response.end("JSON.stringify(result)",'utf-8');
+});
+
+
+// 监听端口，等待连接
+const port = 8765;
+app.listen(port);
+
+// 输出服务器启动日志
+console.log(`Server listening at http://127.0.0.1:${port}`);
